3c1e1fbee825a9d59b470755b62459cef44dc17e
Sumsum Gogoi <104547345+SUGOGOi@users.noreply.github.com>
2024-04-14 00:09:42 +0530

    mmm

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(`${process.env.MONGO_LOCAL}`);
+    const { connection } = await mongoose.connect(`${process.env.MONGO_URL}`);
 
     console.log(`mongodb connected ${connection.host}`);
   } catch (error) {
