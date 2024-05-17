d6d53096d0851435d635bf82329e64cf58ff9b33
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2024-05-17 09:26:40 +0530

    ttt

--- src/utils/sendToken.js
+++ src/utils/sendToken.js
@@ -1,24 +1,23 @@
 import jwt from "jsonwebtoken";
+import cookie from "cookie";
+
 export const sendToken = (res, user, message, statusCode = 200) => {
-  const token = jwt.sign(
-    { _id: user._id, role: user.role },
-    `${process.env.JWT_SECRET}`,
-    {
-      expiresIn: "10d",
-    }
-  );
+  const token = jwt.sign({ _id: user._id }, `${process.env.JWT_SECRET}`, {
+    expiresIn: "10d",
+  });
 
   const options = {
     expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    // secure: true,
-    // sameSite: "none",
-    // priority: High,
-    // domain: "eduverse-client.vercel.app",
+    secure: true,
   };
-  return res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    message,
-    user,
-  });
+  return res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .cookie("role", user.role, options)
+    .json({
+      success: true,
+      message,
+      user,
+    });
 };
